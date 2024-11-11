5bb8769d048f4a3ae3855a4c231d1f6df188f7a3
zenlord22 <connorlonergan@gmail.com>
2024-11-11 11:49:24 -0600

    Add files via upload

---
+++ Algo01/Algo01/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    static void Main()
+    {
+        // Used an input Array to generate diffrent results in the Algorithm. Array size varies to show Algorithm works.
+        Console.WriteLine("Enter the elements of the array separated by spaces:");
+        string input = Console.ReadLine();
+
+        string[] inputArray = input.Split(' ');
+
+        int[] numbers = Array.ConvertAll(inputArray, int.Parse);
+
+        Console.WriteLine("You entered the following array:");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+
+        int firstElement = GetFirstElement(numbers);
+        Console.WriteLine("First element: " + firstElement);
+
+        int sum = SumArray(numbers);
+        Console.WriteLine("Sum of array: " + sum);
+
+        PrintAllPairs(numbers);
+    }
+
+    // O(1) - Constant Time. Program searches for the first element in the array.
+    static int GetFirstElement(int[] array)
+    {
+        return array[0];
+    }
+
+    // O(n) - Linear Time. Program outputs the Sum of Arrary.
+    static int SumArray(int[] array)
+    {
+        int sum = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+    // O(n^2) - Quadratic Time. Program outputs all the pairs in the array.
+    static void PrintAllPairs(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = 0; j < array.Length; j++)
+            {
+                Console.WriteLine($"Pair: ({array[i]}, {array[j]})");
+            }
+        }
+    }
+}
+
---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            Dictionary<int, int> numberMap = new Dictionary<int, int>();
+            Stack<int> numberStack = new Stack<int>();
+            Queue<int> numberQueue = new Queue<int>();
+
+            Console.WriteLine("Enter numbers (type 'done' to finish):");
+
+            string input;
+            int index = 1;
+            while ((input = Console.ReadLine()) != "done")
+            {
+                if (int.TryParse(input, out int number))
+                {
+                    numbers.Add(number);
+                    numberMap[index] = number;
+                    numberStack.Push(number);
+                    numberQueue.Enqueue(number);
+                    index++;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
+
+            // Convert List to Array
+            int[] numberArray = numbers.ToArray();
+
+            // Display the contents of each data structure
+            Console.WriteLine("\nArray:");
+            foreach (var num in numberArray)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine("\n\nDictionary (Map):");
+            foreach (var kvp in numberMap)
+            {
+                Console.WriteLine($"Index: {kvp.Key}, Value: {kvp.Value}");
+            }
+
+            Console.WriteLine("\nStack:");
+            foreach (var num in numberStack)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine("\n\nQueue:");
+            foreach (var num in numberQueue)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
